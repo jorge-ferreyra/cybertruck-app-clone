7596defedfd987def612ce6caff5c3061cccb155
Jorge Ferreyra <jorxfer@gmail.com>
2024-01-23 13:06:08 -0300

    Shapping Models

--- src/components/Model/Car.jsx
+++ src/components/Model/Car.jsx
@@ -1,33 +1,58 @@
 // eslint-disable-next-line react/prop-types
-export function Car ({ gif, title }) {
-  return(
-    <article className='model'>
-      <div className="performance">
-        <div className="title">
-          <h1>{title}</h1>
+export function Car ({ gif, title, range, acceleration, topSpeed, towing, weight }) {
+  if (title === 'REAR-WHEEL DRIVE') {
+    return(
+      <article className='model'>
+        <div className="performance">
+          <div className="title">
+            <h1>{title}</h1>
+         </div>
+          <div className="details">
+           <h3>PERFORMANCE</h3>
+            <p>Range (est.): {range}</p>
+            <p>Acceleration: {acceleration}</p>
+            <p>Top Speed: {topSpeed}</p>
+            <p>Towing: {towing}</p>
+          </div>
         </div>
-        <div className="details">
-          <h3>PERFORMANCE</h3>
-          <p>Range (est.): </p>
-          <p>Acceleration: </p>
-          <p>Top Speed: </p>
-          <p>Towing: </p>
+        <div className="soon">
+          <h3>Details Available in 2025</h3>
         </div>
-        
-      </div>
-      <div className="dimensions">
-        <h3>DIMENSIONS</h3>
-        <p>Weight: </p>
-        <p>Cargo: </p>
-        <p>Wheels: </p>
-        <p>Seating: </p>
-        <h3>CHARGING</h3>
-        <p>Charging Speed: </p>
-        <p>Supercharging Max/Payment Type: </p>
-      </div>
-      <div className="gif">
-        <img src={gif} alt="" />
-      </div>
-    </article>
-  )
+        <div className="gif">
+          <img src={gif} alt="" />
+        </div>
+      </article>
+    )
+  } else {
+    return(
+      <article className='model'>
+        <div className="performance">
+          <div className="title">
+            <h1>{title}</h1>
+          </div>
+          <div className="details">
+            <h3>PERFORMANCE</h3>
+            <p>Range (est.): {range}</p>
+            <p>Acceleration: {acceleration}</p>
+            <p>Top Speed: {topSpeed}</p>
+            <p>Towing: {towing}</p>
+          </div>
+          
+        </div>
+        <div className="dimensions">
+          <h3>DIMENSIONS</h3>
+          <p>Weight: {weight}</p>
+          <p>Cargo: 120.9 CU FT</p>
+          <p>Wheels: {`20"`}</p>
+          <p>Seating: 5 ADULTS</p>
+          <h3>CHARGING</h3>
+          <p>Charging Speed: UP TO 128 MILES ADDED IN 15 MINUTES</p>
+          <p>Supercharging Max/Payment Type: 250KW MAX; PAY PER USE</p>
+        </div>
+        <div className="gif">
+          <img src={gif} alt="" />
+        </div>
+      </article>
+    )
+  }
 }
--- src/components/Model/Models.jsx
+++ src/components/Model/Models.jsx
@@ -11,9 +11,9 @@ export function Models () {
   return(
     <section className='main-section'>
       <div className='main-card'>
-        <Car gif={gif1} title='CYBERBEAST' />
-        <Car gif={gif2} title='ALL WHEEL DRIVE' />
-        <Car gif={gif3} title='REAR-WHEEL DRIVE' />
+        <Car gif={gif1} title='CYBERBEAST' range='320 MILES' acceleration='2.6 SEC 0-60 MPH' topSpeed='130 MPH' towing='11,000 LBS' weight='6,843 LBS' />
+        <Car gif={gif2} title='ALL WHEEL DRIVE' range='340 MILES' acceleration='4.1 SEC 0-60 MPH' topSpeed='112 MPH' towing='11,000 LBS' weight='6,603 LBS' />
+        <Car gif={gif3} title='REAR-WHEEL DRIVE' range='250 MILES' acceleration='6.5 SEC 0-60 MPH ' topSpeed='112 MPH' towing='7,500 LBS' />
         <Link className='link' to='/'>Volver al Home</Link>
         {/*<img src="https://electrek.co/wp-content/uploads/sites/3/2023/11/2021484-00-A_1_2000-e1701390038718.png?w=1600" alt="" />*/}
       </div>
