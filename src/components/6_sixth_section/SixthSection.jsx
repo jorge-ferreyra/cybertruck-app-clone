4c3b49cb9b6093bd7b5e30d87566c6f714c92300
Jorge Ferreyra <jorxfer@gmail.com>
2024-01-29 14:27:45 -0300

    Shapping SixthSection

--- src/components/6_sixth_section/SixthSection.jsx
+++ src/components/6_sixth_section/SixthSection.jsx
@@ -1,10 +1,19 @@
 import './SixthSection.css'
+import './ResponsiveSixthSection.css'
+
+const gifTesla = 'https://media.licdn.com/dms/image/C4E22AQFRIr5N0EsR_g/feedshare-shrink_800/0/1630000942612?e=2147483647&v=beta&t=f0JEk0KAxm89gTiOkhQHQBLS_7tqQkMH1VI6SsIodEc'
 
 export function SixthSection () {
   return(
     <section className='tesla-sixth-section'>
-      <article>
-
+      <article className='doubts-box'>
+        <div className='doubts-title'>
+          <h1>Si ténes alguna duda sobre Tesla o Cybertruck, ¡Mandanos tu consulta!</h1>
+        </div>
+        <div className='doubts-paragraph'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolorem perspiciatis deleniti necessitatibus? Corporis placeat, itaque harum mollitia et eligendi, neque expedita, asperiores quis molestias quibusdam. Quam libero modi dolores.</p>
+          <img src={gifTesla} alt="" />
+        </div>
       </article>
       <form className='tesla-form' action="">
         <h2 className='titleName'>Nombre</h2>
