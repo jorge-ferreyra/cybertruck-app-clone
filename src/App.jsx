6a2ddccff49f4eb1506dd058b1e5cddeb6b982d5
Jorge Ferreyra <jorxfer@gmail.com>
2024-02-02 15:18:26 -0300

    Fixing button in Models

--- src/App.jsx
+++ src/App.jsx
@@ -27,20 +27,34 @@ const appRoutes = [
   },
 ];
 
+const esFlag = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/200px-Bandera_de_Espa%C3%B1a.svg.png'
+const enFlag = 'https://media.istockphoto.com/id/479199262/es/foto/encuadre-completo-imagen-de-inglaterra-bandera.jpg?s=612x612&w=0&k=20&c=SwMP7VR64pbeG-fGCQSbP3e1jYLV0w-79bNyDG5z5Cc='
+
 function App() {
   const [currentLang, setCurrentLang] = useState('es')
+  const [animationButton, setAnimationButton] = useState('es')
 
 
   const handleLangChange = () => {
     const newLang = currentLang === 'es' ? 'en' : 'es'
     setCurrentLang(newLang)
+    if (animationButton == 'es') {
+      document.querySelector('.changeLang').classList.add('clicked')
+      setAnimationButton('en')
+    } else {
+      document.querySelector('.changeLang.clicked').classList.remove('clicked')
+      setAnimationButton('es')
+    }
   }
-  
+
 
   return (
     <>
-      <div>
-        <button className='changeLang' onClick={handleLangChange}>Cambiar Idioma</button>
+      <div className='langContainer'>
+        <img className='flagEs' src={esFlag} alt="" />
+        <button className='changeLang' onClick={handleLangChange}></button>
+        <div className='overlay'></div>
+        <img className='flagEn' src={enFlag} alt="" />
       </div>
       <Router routes={appRoutes} defaultComponent={() => <Page404 lang={currentLang} />} currentLang={currentLang} history={window.history}>
         <Route path={`/${currentLang}/`} render={(routeParams) => <Home routeParams={routeParams} lang={currentLang}/>} />
--- src/components/Model/Models.jsx
+++ src/components/Model/Models.jsx
@@ -1,32 +1,53 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import { Link } from '../Link'
 import { Car } from './Car'
 import { useModelsTech } from '../../hooks/useModelsTech'
 import './Models.css'
 import './ResponsiveModelsES.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const gif1 = 'https://1.bp.blogspot.com/-VEbTm0lKltY/YOHYmUkCkqI/AAAAAAAEVzI/z3UBNcUVZS4Zev1LHP3Pblig8YLAQCTWACLcBGAsYHQ/s600/ClassicLiquidAfricanclawedfrog-size_restricted.gif'
 const gif2 = 'https://www.teslarati.com/wp-content/uploads/2021/10/cybertruck-teaser-2.gif'
 const gif3 = 'https://www.gainsight.com/wp-content/uploads/2014/12/cybertruck.gif'
 
-
+const esFlag = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/200px-Bandera_de_Espa%C3%B1a.svg.png'
+const enFlag = 'https://media.istockphoto.com/id/479199262/es/foto/encuadre-completo-imagen-de-inglaterra-bandera.jpg?s=612x612&w=0&k=20&c=SwMP7VR64pbeG-fGCQSbP3e1jYLV0w-79bNyDG5z5Cc='
 
 export function Models ({ lang }) {
   const [currentLang, setCurrentLang] = useState(lang)
+  const [animationButton, setAnimationButton] = useState(currentLang)
+
+  useEffect(() => {
+    setAnimationButton(lang)
+    if(animationButton === 'es') {
+      document.querySelector('.changeLang2.clicked').classList.remove('clicked')
+    } else {
+      document.querySelector('.changeLang2').classList.add('clicked')
+    }
+  }, [])
 
   const handleLangChange = () => {
     const newLang = currentLang === 'es' ? 'en' : 'es'
     setCurrentLang(newLang)
     history.replaceState(null, '', `/${newLang}/models`);
+    setAnimationButton(newLang)
+    if (animationButton !== 'es') {
+      document.querySelector('.changeLang2.clicked').classList.remove('clicked')
+    } else {
+      document.querySelector('.changeLang2').classList.add('clicked')
+    }
   }
 
   const modelsTech = useModelsTech(currentLang);
   
   return(
     <section className='main-section'>
-      <div>
-        <button className='changeLang' onClick={handleLangChange}>Cambiar Idioma</button>
+      <div className='langContainer2'>
+        <img className='flagEs2' src={esFlag} alt="" />
+        <button className='changeLang2' onClick={handleLangChange}></button>
+        <div className='overlay2'></div>
+        <img className='flagEn2' src={enFlag} alt="" />
       </div>
       <div className='main-card'>
         <Car 
